5b9ee4cb9a6fa0d7328d98c9e37139df7a1e6650
Akshay Sarkar <akshay.sarkar.dbit@gmail.com>
2015-09-19 00:45:08 +0530

    Added Back Key and fixed nav bar issue for small devices
    Added Back Key and fixed nav bar issue for small devices

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,4 +1,15 @@
-var myApp = angular.module('myApp', ['ngRoute','ngAnimate', 'ui.bootstrap']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']),CollegeDetails ;
+
+myApp.controller('appCtrl', [ '$scope', '$window', function($scope, $window) {
+  
+  $scope.isCollapsed = true;
+  $scope.backHome = function () {
+
+      var url = "http://" + $window.location.host + "/#/home";
+      $window.location.href = url;
+  }
+
+}]);
 
 myApp.controller('LoginController', ['$scope', '$window', function($scope, $window) {
     $scope.loginInit = function(loginUser) {
@@ -25,10 +36,10 @@ myApp.controller('LoginController', ['$scope', '$window', function($scope, $wind
             },
             error: function(error) {
                 // Show the error message somewhere
-                if(error.code == 205){
-                  alert("Email Id not registered. PLease enter correct Email id.");
-                }else{
-                  alert("Error: " + error.code + " " + error.message);
+                if (error.code == 205) {
+                    alert("Email Id not registered. PLease enter correct Email id.");
+                } else {
+                    alert("Error: " + error.code + " " + error.message);
                 }
             }
         });
@@ -62,61 +73,58 @@ myApp.controller('RegisterController', ['$scope', function($scope) {
     };
 }]);
 
-myApp.controller('AccordionDemoCtrl', function ($scope) {
-  $scope.oneAtATime = true;
+myApp.controller('AccordionDemoCtrl', function($scope) {
+    $scope.oneAtATime = true;
 
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
+    $scope.groups = [{
+        title: 'Dynamic Group Header - 1',
+        content: 'Dynamic Group Body - 1'
+    }, {
+        title: 'Dynamic Group Header - 2',
+        content: 'Dynamic Group Body - 2'
+    }];
 
-  var query = new Parse.Query(CollegeDetails);
-  query.exists("name");
-  query.find({
-      success: function(results) {
-        //console.log("Successfully retrieved " + results.length + " scores.");
-        $scope.collegeGroups = results;
-        //console.log($scope.collegeGroups);
-      },
-      error: function(error) {
-        console.log("Error: " + error.code + " " + error.message);
-      }
-  });
-
-  // $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  // $scope.addItem = function() {
-  //   var newItemNo = $scope.items.length + 1;
-  //   $scope.items.push('Item ' + newItemNo);
-  // };
-
-  $scope.status = {
-    isFirstOpen: false,
-    isFirstDisabled: false
-  };
-
-  $scope.collegeDetailFunc = function (CollegeDetailModal) {
-    var collegeDetail = CollegeDetails.createCollege(CollegeDetailModal);
-    collegeDetail.save(null, {
-        success: function(college) {
-          // Execute any logic that should take place after the object is saved.
-          alert('College Created: ' + college.name);
-          CollegeDetailModal ='';
+    var query = new Parse.Query(CollegeDetails);
+    query.exists("name");
+    query.find({
+        success: function(results) {
+            //console.log("Successfully retrieved " + results.length + " scores.");
+            $scope.collegeGroups = results;
+            //console.log($scope.collegeGroups);
         },
-        error: function(collegeDetail, error) {
-          // Execute any logic that should take place if the save fails.
-          // error is a Parse.Error with an error code and message.
-          alert('Failed to create new object, with error code: ' + error.message);
+        error: function(error) {
+            console.log("Error: " + error.code + " " + error.message);
         }
     });
 
-  }
+    // $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    // $scope.addItem = function() {
+    //   var newItemNo = $scope.items.length + 1;
+    //   $scope.items.push('Item ' + newItemNo);
+    // };
+
+    $scope.status = {
+        isFirstOpen: false,
+        isFirstDisabled: false
+    };
+
+    $scope.collegeDetailFunc = function(CollegeDetailModal) {
+        var collegeDetail = CollegeDetails.createCollege(CollegeDetailModal);
+        collegeDetail.save(null, {
+            success: function(college) {
+                // Execute any logic that should take place after the object is saved.
+                alert('College Created: ' + college.name);
+                CollegeDetailModal = '';
+            },
+            error: function(collegeDetail, error) {
+                // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                alert('Failed to create new object, with error code: ' + error.message);
+            }
+        });
+
+    }
 });
 
 myApp.config(function($routeProvider, $locationProvider) {
@@ -154,37 +162,38 @@ myApp.config(function($routeProvider, $locationProvider) {
 
     // configure html5 to get links working on jsfiddle
     //$locationProvider.html5Mode(true);
-});
 
-/* Navigation Item*/
-var navEle = angular.element(document.querySelector('#nav-tab'));
-navEle.on('click', function(evt) {
-    angular.element(document.querySelector("li.active")).removeClass("active");
-    angular.element(evt.target).parent().addClass("active");
-});
+    /* Navigation Item*/
+    var navEle = angular.element(document.querySelector('#nav-tab'));
+    navEle.on('click', function(evt) {
+        angular.element(document.querySelector("li.active")).removeClass("active");
+        angular.element(evt.target).parent().addClass("active");
+    });
 
-/*Parse Object Create */
 
-// Simple syntax to create a new subclass of Parse.Object.
-//var CollegeDetails = Parse.Object.extend("CollegeDetails");
+    /*Parse Object Create */
 
-// A complex subclass of Parse.Object
-var CollegeDetails = Parse.Object.extend("CollegeDetails", {
-  // // Instance methods
-  // hasSuperHumanStrength: function () {
-  //   return this.get("strength") > 18;
-  // },
-  // Instance properties go in an initialize method
-  initialize: function (attrs, options) {
-    this.collegeType = "Law College";
-  }
-}, {
-  // Class methods
-  createCollege: function(CollegeDetailModal) {
-     var college = new CollegeDetails();
-     college.set("name", CollegeDetailModal.collegeName);
-     college.set("contact", CollegeDetailModal.collegeContact);
-     college.set("homepage", CollegeDetailModal.collegeHomepage);
-     return college;
-  }
+    // Simple syntax to create a new subclass of Parse.Object.
+    //var CollegeDetails = Parse.Object.extend("CollegeDetails");
+
+    // A complex subclass of Parse.Object
+    CollegeDetails = Parse.Object.extend("CollegeDetails", {
+        // // Instance methods
+        // hasSuperHumanStrength: function () {
+        //   return this.get("strength") > 18;
+        // },
+        // Instance properties go in an initialize method
+        initialize: function(attrs, options) {
+            this.collegeType = "Law College";
+        }
+    }, {
+        // Class methods
+        createCollege: function(CollegeDetailModal) {
+            var college = new CollegeDetails();
+            college.set("name", CollegeDetailModal.collegeName);
+            college.set("contact", CollegeDetailModal.collegeContact);
+            college.set("homepage", CollegeDetailModal.collegeHomepage);
+            return college;
+        }
+    });
 });
